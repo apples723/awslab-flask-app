8726b539200153ff0a0b240ab470c4b4f6732d83
Grant Siders <gsiders@sorenson.com>
2023-09-21 19:11:44 +0000

    fix param path

--- helpers.py
+++ helpers.py
@@ -14,12 +14,11 @@ def get_instance_name(az=None, prefix=None):
     instance_name = f"{prefix}-{az}.slalom.awslab.cloud"
     return instance_name
 
-
-def get_status_from_ssm(param_name="/slalom-awslab-cloud/flask-app/http-satus-code"):
+def get_status_from_ssm(param_name="/slalom-awslab-cloud/flask-app/http-status-code"):
     status = ssmc.get_parameter(Name=param_name)
     return status["Parameter"]["Value"]
 
 def update_status_in_ssm(status, param_name="/slalom-awslab-cloud/flask-app/http-status-code"):
-    update_command = ssmc.put_parameter(Name=param_name, Value=status, Overwwrite=True)
+    update_command = ssmc.put_parameter(Name=param_name, Value=status, Overwrite=True)
     update_command_status = update_command["ResponseMetadata"]["HTTPStatusCode"]
     return update_command_status

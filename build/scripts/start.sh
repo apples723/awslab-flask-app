5eb2655550727e8ebd7bf668858a0688f7cb73f4
Grant Siders <gsiders@sorenson.com>
2023-09-21 21:09:09 +0000

    add volume creation to start up/fix yaml name

--- build/scripts/start.sh
+++ build/scripts/start.sh
@@ -1,3 +1,6 @@
+sudo docker network create caddy
+sudo docker volume create caddy_data
+sudo docker volume create caddy_config
 sudo docker-compose -f /home/ubuntu/awslab-flask-app/docker-compose.yaml up -d
 echo "waiting for flask app to start..."
 sleep 10

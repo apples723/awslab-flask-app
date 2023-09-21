c8fed63f4b740935c71809193bb10e1e4af7aa56
Grant Siders <gsiders@sorenson.com>
2023-09-21 21:01:55 +0000

    bump docker image version

--- build/scripts/start.sh
+++ build/scripts/start.sh
@@ -1,4 +1,4 @@
-sudo docker-compose -f /home/ubuntu/dev/awslab-flask-app/docker-compose.yaml up -d
+sudo docker-compose -f /home/ubuntu/awslab-flask-app/docker-compose.yaml up -d
 echo "waiting for flask app to start..."
 sleep 10
-sudo docker-compose -f /home/ubuntu/dev/awslab-flask-app/caddy/docker-compose.yaml up -d
+sudo docker-compose -f /home/ubuntu/awslab-flask-app/caddy/docker-compose.yaml up -d

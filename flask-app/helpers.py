85e1ebdb8988c7d9fa68bfd09d7e66af724b3fd7
Grant Siders <gsiders@sorenson.com>
2023-10-03 08:17:41 -0600

    fix whereami return obj

--- flask-app/helpers.py
+++ flask-app/helpers.py
@@ -6,7 +6,7 @@ def get_instance_az_letter(az=None):
     az = requests.get("http://169.254.169.254/latest/meta-data/placement/availability-zone", timeout=2).text[-1]
     return az
 def get_instance_region():
-    region = requests.get("http://169.254.169.254/latest/meta-data/placement/region", timeout=2)
+    region = requests.get("http://169.254.169.254/latest/meta-data/placement/region", timeout=2).text
     return region
 def get_instance_name(az=None, prefix=None):
     if prefix == None:
